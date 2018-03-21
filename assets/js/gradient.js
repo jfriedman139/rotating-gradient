82e025d5d79c82f463a634c504dbe5be6da8eb98
Josh Friedman <jfriedman@Joshs-MBP.fios-router.home>
2018-03-21 11:24:02 -0400

    compatability

--- assets/js/gradient.js
+++ assets/js/gradient.js
@@ -10,10 +10,16 @@ $(document).ready(function(){
 	//Run function every 20 ms	
 	setInterval(function() {
 
-		//if hover is still active incriment the degree of the angle of the gradient by 1
+		//if hover is still active incriment / decrement the degree of the angle of the gradient by 1
 		if (hover) {
-			outer.css('background', "linear-gradient(" + outerDeg + "deg, #23074d, #cc5333)");
-			circle.css('background', "linear-gradient(" + circleDeg + "deg, #23074d, #cc5333)");
+			outer.css({
+				"background": "linear-gradient(" + outerDeg + "deg, #23074d, #cc5333)",
+				"background": "-webkit-linear-gradient(" + outerDeg + "deg, #23074d, #cc5333)"
+			});
+			circle.css({
+				"background": "linear-gradient(" + circleDeg + "deg, #23074d, #cc5333)",
+				"background": "-webkit-linear-gradient(" + circleDeg + "deg, #23074d, #cc5333)"
+			});
 			outerDeg += 1;
 			circleDeg -= 1;
 		}
